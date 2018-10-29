a0ffe6dfe7841d8ab353701922d1a15f4fc9e5b3
Terence WODLING <terencewodling@gmail.com>
2018-10-29 16:06:33 +0100

    ApiRest Basic

---
+++ compil.cmd
@@ -0,0 +1,8 @@
+cd users\teren\eclipse-workspace\ApiRest
+mvn clean install
+cd.. 
+cd..
+cd..
+cd..
+cd..
+copy C:\Users\teren\eclipse-workspace\ApiRest\target\ApiRest.war D:\Tomcat\apache-tomcat-8.5.33\webapps
---
+++ deploy.cmd
@@ -0,0 +1,1 @@
+copy C:\Users\teren\eclipse-workspace\ApiRest\target\ApiRest.war C:\"Program Files"\"Apache Software Foundation"\"Tomcat 7.0"\webapps
